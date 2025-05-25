c1008f92905be35a089c260314dfab72c75d6469
Ladislav Vondráček <lawondyss@gmail.com>
2025-05-25 20:15:13 +0200

    Fixed CSS for overflowing output from window size

--- src/App.svelte
+++ src/App.svelte
@@ -16,7 +16,7 @@
             return {output, error: null, position: null}
 
         } catch (err) {
-            let error = new String(err).replace('SyntaxError: ', '').replace(' is not valid JSON', '').replace(/ \(line[^\)]+\)/, '')
+            let error = String(err).replace('SyntaxError: ', '').replace(' is not valid JSON', '').replace(/ \(line[^)]+\)/, '')
             const regex = /(\d+)$/
             const position = parseInt(error.match(regex)?.at(0) ?? '0')
 
@@ -142,16 +142,16 @@
 
 
 <section>
-    <header style="border-top-left-radius: var(--radius)">
+    <header style:border-top-left-radius="var(--radius)">
         <div id="cursor">Cursor position: {cursor ?? 'none'}</div>
     </header>
-    <textarea bind:value={input} style="border-bottom-left-radius: var(--radius)"></textarea>
+    <textarea bind:value={input} style:border-bottom-left-radius="var(--radius)"></textarea>
 </section>
 
 <section class="flexible">
-    <header style="border-top-right-radius: var(--radius)">
+    <header style:border-top-right-radius="var(--radius)">
         <input id="filter" type="text" bind:value={filter} class:invalid={filteredOutput === FilterError} placeholder="path / filter"/>
-        <button popovertarget="help">
+        <button popovertarget="help" aria-label="Show help">
             <svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 24 24">
                 <path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zM7.92 9.234v.102a.5.5 0 0 0 .5.5h.997a.499.499 0 0 0 .499-.499c0-1.29.998-1.979 2.34-1.979 1.308 0 2.168.689 2.168 1.67 0 .928-.482 1.359-1.686 1.91l-.344.154C11.379 11.54 11 12.21 11 13.381v.119a.5.5 0 0 0 .5.5h.997a.499.499 0 0 0 .499-.499c0-.516.138-.723.55-.912l.345-.155c1.445-.654 2.529-1.514 2.529-3.39v-.103c0-1.978-1.72-3.441-4.164-3.441-2.478 0-4.336 1.428-4.336 3.734zm2.58 7.757c0 .867.659 1.509 1.491 1.509.85 0 1.509-.642 1.509-1.509 0-.867-.659-1.491-1.509-1.491-.832 0-1.491.624-1.491 1.491z"/>
             </svg>
@@ -189,6 +189,13 @@
         flex-direction: column;
         gap: var(--gap-small);
 
+        &.flexible {
+            flex: 1;
+            min-width: 0;
+            width: 100%;
+            overflow: hidden;
+        }
+
         header {
             flex: 0 0 auto;
             display: flex;
@@ -206,7 +213,7 @@
                 align-self: stretch;
                 flex: 1;
                 font-size: 1rem;
-                font-family: var(--font-mono);
+                font-family: var(--font-mono), monospace;
             }
 
             button {
@@ -240,10 +247,12 @@
             flex: 1;
             display: flex;
             padding: var(--gap);
-            font-family: var(--font-mono);
+            font-family: var(--font-mono), monospace;
             line-height: 1.2;
             overflow: auto;
             background-color: var(--dark-2);
+            min-width: 0;
+            width: 100%;
 
             .msg {
                 display: block;
@@ -290,6 +299,9 @@
     .string {
         color: var(--string);
         text-wrap: nowrap;
+        max-width: 100%;
+        overflow: hidden;
+        text-overflow: ellipsis;
     }
 
     .number {
